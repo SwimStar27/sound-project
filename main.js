3d9603f6118c5616012c5a91ea27a91e7017d580
SwimStar27 <94149915+SwimStar27@users.noreply.github.com>
2022-05-08 15:26:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/VmlWsviiW/model.json',modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
