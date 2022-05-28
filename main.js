c43e9b3340352181f9258ac3c0cfb6ee3004e886
SwimStar27 <94149915+SwimStar27@users.noreply.github.com>
2022-05-28 14:32:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,50 @@ function start(){
 function modelReady(){
     classifier.classify(gotResults);
 }
+
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        var runnr=Math.floor(Math.random()*255)+1;
+        var dinnr=Math.floor(Math.random()*255)+1;
+        var winnr=Math.floor(Math.random()*255)+1;
+        document.getElementById("result").innerHTML='The Sound Heard Is: '+  (results[0].label);
+        document.getElementById("confidence").innerHTML='The Accuracy Is: '+ (results[0].confidence*100).toFixed(2)+" % ";
+        document.getElementById("result").style.color="rgb("+runnr+","+dinnr+","+winnr+")";
+        document.getElementById("confidence").style.color="rgb("+runnr+","+dinnr+","+winnr+")";
+
+        var img1=document.getElementById("alien1");
+        var img2=document.getElementById("alien2");
+        var img3=document.getElementById("alien3");
+        var img4=document.getElementById("alien4");
+        
+        if(results[0].label=="Background Noise"){
+            img1.src='alien1.png';
+            img2.src='alien2.png';
+            img3.src='alien3.png';
+            img4.src='bob.gif';
+        }
+
+       else if(results[0].label=="Talking"){
+            img1.src='alien1.gif';
+            img2.src='alien2.png';
+            img3.src='alien3.png';
+            img4.src='bob.png';
+        }
+        else if(results[0].label=="Calpping"){
+            img1.src='alien1.png';
+            img2.src='alien2.gif';
+            img3.src='alien3.png';
+            img4.src='bob.png';
+        }
+        else if(results[0].label=="Water"){
+            img1.src='alien1.png';
+            img2.src='alien2.png';
+            img3.src='alien3.gif';
+            img4.src='bob.png';
+        }
+    }
+}
